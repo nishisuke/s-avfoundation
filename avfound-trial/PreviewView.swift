45ef59ecde6430702533839f4f14d6b34722c4e3
nishisuke <nishisuke13+git@gmail.com>
2018-12-23 11:08:21 +0900

    Fix preview view

--- avfound-trial/PreviewView.swift
+++ avfound-trial/PreviewView.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    /// Convenience wrapper to get layer as its statically known type.
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+}
